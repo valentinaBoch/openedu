fc990ab148daf8fd5b3f15ccd7af1863897f8251
vally <vally.1209@gmail.com>
2016-10-15 20:30:30 +0300

    sort2

--- algorithm/lk_2/sort2.cpp
+++ algorithm/lk_2/sort2.cpp
@@ -1,54 +1,86 @@
 #include <iostream>
-#include <conio.h>
+#include <fstream>
 
 using namespace std;
 
-#define maxn 100
+//#define maxn 100
 
-int a[maxn];
+void merge(int * arr, int l, int r) 
+{
+	ofstream fout ("sort.out", std::ios_base::app);
+	if (r == l)
+	{	
+		fout << l << " " << r << " " << arr[l] << " " << arr [r] << endl;
+        	return;
+ 	}   	
+	if (r - l == 0) 
+	{ 
+        	if (arr[r] < arr[l])
+            		swap(arr[r], arr[l]);
+        	return;
+    	}
 
-int n;
+	int m = (r + l) / 2;
+    	merge(arr, l, m);
+    	merge(arr, m + 1, r);
+    	int buf[r];
+    	int xl = l;
+    	int xr = m + 1;
+    	int cur = 0;
+//	int count = 0;
+    	while (r - l + 1 != cur) 
+	{
+		fout << xl << " " << xr << " ";
+        	if (xl > m)
+            	{
+			fout  << arr[xl] << " " << arr [xr] << endl;
+			buf[cur++] = arr[xr++];
+		}
+        	else 
+			if (xr > r)
+            		{	
+				fout << arr[xl] << " " << arr [xr] << endl;
+				buf[cur++] = arr[xl++];	
+        		}
+			else 
+				if (arr[xl] > arr[xr])
+				{
+					fout << arr[xr] << " " << arr [xl] << endl;
+            				buf[cur++] = arr[xr++];
+				}
+        			else 
+				{
+					fout << arr[xl] << " " << arr [xr] << endl;
+ 					buf[cur++] = arr[xl++];
 
-void merge(int l, int r) {
-    if (r == l)
-        return;
-    if (r - l == 1) { 
-        if (a[r] < a[l])
-            swap(a[r], a[l]);
-        return;
-    }
-    int m = (r + l) / 2;
-    merge(l, m);
-    merge(m + 1, r);
-    int buf[maxn];
-    int xl = l;
-    int xr = m + 1;
-    int cur = 0;
-    while (r - l + 1 != cur) {
-        if (xl > m)
-            buf[cur++] = a[xr++];
-        else if (xr > r)
-            buf[cur++] = a[xl++];
-        else if (a[xl] > a[xr])
-            buf[cur++] = a[xr++];
-        else buf[cur++] = a[xl++];
-
-    }
-    for (int i = 0; i < cur; i++)
-        a[i + l] = buf[i];
+				}
+    	}
+    	for (int i = 0; i < cur; i++)
+        	arr[i + l] = buf[i]; 
+//	for (int i = 1; i <= r; i++) cout << arr[i] << " ";
+	fout.close();
+	
 }
 
-int main() {    
-    cin >> n;
+int main() 
+{    
+	int n;
+        ifstream fin ("sort.in");
+        fin >> n;
 
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
+        int i = 1;
+        int *array = new int[100];
+        for (i; i <= n; i++)
+                fin >> array[i];
+        fin.close();
 
-    merge(0, n - 1);
+	merge(array, 1, n);
 
-    for (int i = 0; i < n; i++)
-        cout << a[i] << " ";
-
-    getch();
-    return 0;
+//	for (int i = 1; i <= n; i++)
+//        	cout << array[i] << " ";
+	ofstream fout ("sort.out", std::ios_base::app);
+	for (int i = 1; i <= n; i++)
+              fout << array[i] << " ";
+	fout.close();
+	return 0;
 }
